1d33a3fde3f9d16e9a0cd54e14ff3565d2990064
TerenceW-0511 <terencew0511@gmail.com>
2026-02-16 15:09:23 -0500

    Far auton pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutonFar9.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutonFar9.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.pedroPathing; // make sure this aligns with class location
+package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
