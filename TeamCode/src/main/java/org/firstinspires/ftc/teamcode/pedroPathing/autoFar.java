6deab282c69fd6863caccad66d476f8cfa3d303d
TerenceW-0511 <terencew0511@gmail.com>
2026-02-19 23:24:53 -0500

    Far auton pathing changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autoFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autoFar.java
@@ -8,18 +8,22 @@ import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 @Autonomous(name = "Example Auto", group = "Blue")
 public class autoFar extends OpMode {
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
     private int pathState;
+    private Methods intakePID,transferPID,flywheelPID,methods;
+    private Hardware robot;
 
 
     private final Pose startPose = new Pose(57.88372093023256, 8.46511627906977, Math.toRadians(180)); // Start Pose of our robot.
     private final Pose GrabPlayerZone = new Pose(10.325581395348829, 8.46511627906977, Math.toRadians(180)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     private final Pose ScorePlayerZone = new Pose(58.527906976744184, 15.406976744186043, Math.toRadians(180)); // Highest (First Set) of Artifacts from the Spike Mark.
-    private final Pose GrabLastChain = new Pose(21.46511627906974, 37.27906976744186, Math.toRadians(180)); // Middle (Second Set) of Artifacts from the Spike Mark.
-    private final Pose ControlToGrabLastChain = new Pose(55.1, 37.8); // Lowest (Third Set) of Artifacts from the Spike Mark.
+    private final Pose GrabLastChain = new Pose(17.069767441860442, 37.06976744186046, Math.toRadians(180)); // Middle (Second Set) of Artifacts from the Spike Mark.
+    private final Pose ControlToGrabLastChain = new Pose(55.06976744186045, 37.77906976744185); // Lowest (Third Set) of Artifacts from the Spike Mark.
 
     private final Pose ScoreLastChain = new Pose(49.3953488372093, 8.5, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
 
@@ -75,10 +79,20 @@ public class autoFar extends OpMode {
     public void autonomousPathUpdate() {
         switch (pathState) {
             case 0:
-                setPathState(1);
+                if (robot.flywheel1.getVelocity() + robot.flywheel2.getVelocity() /2 >= Values.flywheel_Values.flywheelTarget && outtake(3)) {
+                        setPathState(1);
+                }
+                break;
             case 1:
+                move();
                 follower.followPath(grabPlayer);
-                setPathState(2);
+                setPathState(69);
+                break;
+            case 69:
+                if (follower.getPathCompletion()>5) {
+                    intake();
+                    setPathState(2);
+                }
                 break;
             case 2:
 
@@ -87,38 +101,42 @@ public class autoFar extends OpMode {
             - Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
             - Robot Position: "if(follower.getPose().getX() > 36) {}"
             */
-
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Preload */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(ScorePlayer);
+                    setPathState(37);
+                }
+                break;
+            case 37:
+                if (outtake(3)){
                     setPathState(3);
                 }
-                break;
             case 3:
+                move();
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position */
                 if(!follower.isBusy()) {
-                    /* Grab Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
                     follower.followPath(GrabLast);
-                    setPathState(4);
+                    setPathState(44);
                 }
                 break;
+            case 44:
+                if (follower.getPathCompletion()>5){
+                    intake();
+                    setPathState(4);
+                }
             case 4:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
-                    /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(ScoreLast);
+                    setPathState(58);
+                }
+            case 58:
+                if (outtake(3)){
                     setPathState(5);
                 }
                 break;
+            case 9:
             case 5:
-            case 7:
+                move();
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup2Pose's position */
                 if(!follower.isBusy()) {
                     /* Grab Sample */
@@ -128,18 +146,27 @@ public class autoFar extends OpMode {
                     nextPath();
                 }
                 break;
+            case 10:
             case 6:
-            case 8:
+                if (follower.getPathCompletion()>.5){
+                    intake();
+                    nextPath();
+                }
+            case 11:
+            case 7:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
                     /* Score Sample */
-
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
                     follower.followPath(Scoreplayer2);
                     nextPath();
                 }
                 break;
-            case 9:
+            case 12:
+            case 8:
+                if (outtake(3)){
+                    nextPath();}
+                break;
+            case 13:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if(!follower.isBusy()) {
                     /* Set the state to a Case we won't use or define, so it just stops running an new paths */
@@ -148,11 +175,41 @@ public class autoFar extends OpMode {
                 break;
         }
     }
+    public void intake(){
+        follower.setMaxPower(0.4);
+        robot.limiter.setPosition(Values.LIMITER_CLOSE);
+        robot.intake.setPower(1);
+        robot.transfer.setPower(.8);
 
+    }
+
+    public void move(){
+        robot.limiter.setPosition(Values.LIMITER_OPEN);
+        follower.setMaxPower(1);
+        robot.intake.setPower(0);
+        robot.transfer.setPower(0);
+    }
+    public boolean outtake(double time){
+        double avgFlywheel = (robot.flywheel1.getVelocity() + robot.flywheel2.getVelocity()) / 2.0;
+        double rpmError = Math.abs(avgFlywheel - Values.flywheel_Values.flywheelTarget);
+        if (pathTimer.getElapsedTimeSeconds()>0.2) {
+            robot.intake.setPower(1);
+            robot.transfer.setPower(1);
+        }
+        if (pathTimer.getElapsedTimeSeconds()>time){
+            return true;
+        }
+        return false;
+    }
     /** These change the states of the paths and actions. It will also reset the timers of the individual switches **/
     public void setPathState(int pState) {
         pathState = pState;
         pathTimer.resetTimer();
+    }
+
+    public void nextPath(){
+        pathTimer.resetTimer();
+        pathState+=1;
     }
 
     /**
@@ -164,16 +221,30 @@ public class autoFar extends OpMode {
         // These loop the movements of the robot, these must be called continuously in order to work
         follower.update();
         autonomousPathUpdate();
+
         Values.autonFollowerX = follower.getPose().getX();
         Values.autonFollowerY = follower.getPose().getY();
         Values.autonHeading = Math.toDegrees(follower.getHeading());
 
+        robot.hood1.setPosition(0.5);
+
+        double turretEncoder = -robot.intake.getCurrentPosition();
+
+        double targetTurret = methods.AutoAim(follower.getPose(), robot.ll);
+        Values.turretPos = methods.turretPID(turretEncoder, targetTurret + Values.turretOverride);
+        robot.turret1.setPosition(Values.turretPos);
+        robot.turret2.setPosition(Values.turretPos);
+
+        Values.flywheel_Values.flywheelTarget = methods.flywheelControl(follower,robot.hood1.getPosition());
+        flywheelPID.flywheelFFTele(robot.flywheel1, robot.flywheel2, Values.flywheel_Values.flywheelTarget);
 
         // Feedback to Driver Hub for debugging
         telemetry.addData("path state", pathState);
         telemetry.addData("x", follower.getPose().getX());
         telemetry.addData("y", follower.getPose().getY());
         telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("avg velo", robot.flywheel1.getVelocity() + robot.flywheel2.getVelocity() / 2);
+        telemetry.addData("target velo", Values.flywheel_Values.flywheelTarget);
         telemetry.update();
     }
 
@@ -186,6 +257,13 @@ public class autoFar extends OpMode {
         opmodeTimer = new Timer();
         opmodeTimer.resetTimer();
 
+        robot = new Hardware(hardwareMap);
+        robot.intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intakePID = new Methods();
+        transferPID = new Methods();
+        flywheelPID = new Methods();
+        methods = new Methods();
 
         follower = Constants.createFollower(hardwareMap);
         buildPaths();
@@ -198,6 +276,13 @@ public class autoFar extends OpMode {
      **/
     @Override
     public void init_loop() {
+        robot.limiter.setPosition(Values.LIMITER_CLOSE);
+        double turretEncoder = -robot.intake.getCurrentPosition();
+
+        double targetTurret = methods.AutoAim(follower.getPose(), robot.ll);
+        Values.turretPos = methods.turretPID(turretEncoder, targetTurret + Values.turretOverride);
+        robot.turret1.setPosition(Values.turretPos);
+        robot.turret2.setPosition(Values.turretPos);
     }
 
     /**
@@ -209,10 +294,7 @@ public class autoFar extends OpMode {
         opmodeTimer.resetTimer();
         setPathState(0);
     }
-    public void nextPath(){
-        pathState += 1;
-        pathTimer.resetTimer();
-    }
+
     /**
      * We do not use this because everything should automatically disable
      **/
